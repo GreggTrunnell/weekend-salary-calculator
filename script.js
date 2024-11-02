f93a66247481cf8c792726137d2ff5d1b3b6c82a
Gregg Trunnell <gggtrunnell@gmail.com>
2024-11-02 17:47:09 -0500

    creating add employee function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('weekend project')
+
+// Write function to add affirmations to list in HTML table
+// function addAffirmation(event) {
+
+//     // Stop default behavior of refreshing the page when there's a button elem inside a form element
+//     event.preventDefault();
+  
+//     // Get the inputs and table
+//     const affirmation = document.querySelector("#affirmationInput").value;
+//     const author = document.querySelector("#authorInput").value;
+//     let tableBody = document.querySelector("#affirmationList tbody");
+
+//use event because some one is activating the event
+function addEmployee(event){
+    // Stop default behavior of refreshing the page when there's a button elem inside a form element
+  event.preventDefault();
+    // need to create five variables to take in the data for the employee
+    //document accesses the html. get elementById will grab that specific Id 
+    const firstName=document.getElementById('#firstNameInput').value;
+  
+}
+addEmployee()
